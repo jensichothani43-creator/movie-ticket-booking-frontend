43845a5194fdb850146baf22c3fbfbd578a43a48
Jensi Chothani <jensichothani43@gmail.com>
2026-08-24 15:23:18 +0530

    Add axios instance for API requests

---
+++ src/api/axios.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+const api = axios.create({
+  baseURL: "https://movie-ticket-booking-frontend-oouw-n3unsp159-movie21.vercel.app/v1",
+});
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+export default api;
