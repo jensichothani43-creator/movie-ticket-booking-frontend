0c3768c26b840af5ffb96357c956d04e243e716e
Jensi Chothani <jensichothani43@gmail.com>
2026-08-24 17:57:56 +0530

    Update API base URL for movie ticket booking

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const api = axios.create({
-baseURL: "https://movie-ticket-booking-frontend-oouw-iivps90nk-movie21.vercel.app",    
+  baseURL: "https://movie-ticket-booking-frontend-oouw.vercel.app/v1",
 });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
